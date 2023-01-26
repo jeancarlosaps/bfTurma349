43026de65629bae42f2ed56b3cd40bc158531322
Ellington Cavalcante <ellingtoncavalcante@192.168.0.7>
2023-01-25 21:27:01 -0300

    Botão e label com auto layout

--- BFTurma349/BFTurma349/ViewController.swift
+++ BFTurma349/BFTurma349/ViewController.swift
@@ -8,9 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tappedButton: UIButton!
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpButton()
+        setUpTextField()
+    }
+    
+    func setUpButton() {
+        
+        tappedButton.titleLabel?.text = "Calcular"
+        tappedButton.titleLabel?.textColor = .white
+    }
+    
+    func setUpTextField() {
+        
+        
+        textField.placeholder = "Digite os preços!"
+        textField.textAlignment = .center
+        let placeholderText = NSAttributedString(string: "Digite os preços!", attributes: [NSAttributedString.Key.foregroundColor:UIColor.white, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 23)])
+        
+        
+        textField.attributedPlaceholder = placeholderText
+        textField.keyboardType = .numberPad
+        
+        
     }
 }
