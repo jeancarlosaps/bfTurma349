e1fe03ef1bc8768feecc6e50cc933cd0b5130996
wcovi <wcovi@yahoo.com.br>
2023-01-29 20:26:57 -0300

    Função do cálculo criada
    Adicionamos a função de cálculo do combustível que estava faltando a partir da branch task01, que nosso grupo tinha feito.
    
    Co-Authored-By: Rodrigo Cerqueira <15165482+rudrigaum@users.noreply.github.com>
    Co-Authored-By: Yan Alejandro <61808807+YanDevIOS@users.noreply.github.com>
    Co-Authored-By: Lucas Gomes <85777959+LucasGomeszx@users.noreply.github.com>
    Co-Authored-By: Vitor Ernane <86169767+vitorernane@users.noreply.github.com>

--- BFTurma349/BFTurma349/ViewController.swift
+++ BFTurma349/BFTurma349/ViewController.swift
@@ -12,10 +12,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var gasolinaTextField: UITextField!
     @IBOutlet weak var alcoolTextField: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func calculateBestFuel(_ sender: Any) {
+        let gasolinaDouble = Double(gasolinaTextField.text!)
+        let alcoolDouble = Double(alcoolTextField.text!)
+        guard let gasolinaDouble = gasolinaDouble, let alcoolDouble = alcoolDouble else { return }
+        let reference = (gasolinaDouble) * 0.7
+        if reference < (alcoolDouble) {
+            result.text = "Vale mais a pena abastecer com gasolina!"
+        } else {
+            result.text = "Vale mais a pena abastecer com álcool!"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         gasolinaTextField.delegate = self
         alcoolTextField.delegate = self
     }
@@ -25,11 +37,12 @@ class ViewController: UIViewController {
 extension ViewController: UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
         textField.layer.borderWidth = 2
-        gasolinaTextField.layer.borderColor = UIColor.red.cgColor
-        alcoolTextField.layer.borderColor = UIColor.green.cgColor
+        gasolinaTextField.layer.borderColor = UIColor.blue.cgColor
+        alcoolTextField.layer.borderColor = UIColor.blue.cgColor
     }
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
         textField.layer.borderWidth = 0
     }
+
     
 }
